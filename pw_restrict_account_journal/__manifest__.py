ffde3aa98000cb7116238e128f34e1c35c6b5831
Musaab $aip <67084214+musaab123@users.noreply.github.com>
2024-05-25 12:45:16 +0400

    Add files via upload

---
+++ pw_restrict_account_journal/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Journal Restriction on Users",
+    "version": "17.0.0.1",
+    'summary': """
+        This module is allow you to restrict account journal to the specific users to access allowed journals only | Journal Restriction for users | Journal restricted users | Journal restriction by user | User journal restriction""",
+    'description': """
+This module is allow you to restrict account journal to the specific users to access allowed journals only
+        """,    
+    "category": "Accounting",
+    'author': "Preway IT Solutions",
+    "sequence": 2,
+    "depends" : ['account'],
+    "data" : [
+        'security/account_security.xml',
+        'views/res_user_view.xml',
+    ],
+    'price': 10.0,
+    'currency': 'EUR',
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+    'live_test_url': 'https://youtu.be/xmJqnPFyy30',
+    "images":["static/description/Banner.png"],
+}
